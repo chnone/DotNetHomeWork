96b2647e3207aa71c8feeea783e96b62f5c949b3
chnone <76799673+chnone@users.noreply.github.com>
2021-04-29 20:54:01 +0800

    Add files via upload

---
+++ Cswork4/Clock/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    public delegate void ClockHandler(object sender, TimeEventArgs args); 
+
+    public class TimeEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    public class SetClock
+    {
+        private int hour, minute, second;
+        private int AlarmHour, AlarmMinute;
+        private int AlarmSecond = 0;
+        public event ClockHandler OnTick;
+        public event ClockHandler OnAlarm;
+        public SetClock(int hour, int minute)
+        {
+            if (hour < 24 && minute < 60)
+            {
+                this.hour = hour;
+                this.minute = minute;
+                this.second = 0;
+            }
+            else
+            {
+                Console.WriteLine("输入错误，时间改为0:0:0");
+                this.hour = 0;
+                this.minute = 0;
+                this.second = 0;
+            }
+        }
+        public void GetClock()
+        {
+            Console.WriteLine("设置的时钟时间为" + hour + ":" + minute + ":" + second);
+        }
+        public void SetAlarm(int hour, int minute)
+        {
+            AlarmHour = hour;
+            AlarmMinute = minute;
+        }
+        public void GetAlarm()
+        {
+            Console.WriteLine("设置的闹钟时间为" + AlarmHour + ":" + AlarmMinute);
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                second++;
+                if (second == 60)
+                {
+                    second = 0;
+                    minute++;
+                }
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+                }
+                if (hour == 24)
+                {
+                    hour = 0;
+                }
+
+                TimeEventArgs args = new TimeEventArgs() { Hour = hour, Minute = minute, Second = second };
+                
+                OnTick(this, args);
+                if (hour == AlarmHour && minute == AlarmMinute && second == AlarmSecond)
+                {
+                    OnAlarm(this, args);
+                }
+                
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+
+    public class MyClock
+    {
+        public SetClock myClock;
+        public MyClock(int hour, int minute)
+        {
+            myClock = new SetClock(hour, minute);
+            myClock.OnTick += OnTick1;
+            myClock.OnAlarm += OnAlarm1;
+        }
+        void OnTick1(object sender, TimeEventArgs args)
+        {
+            Console.WriteLine("Tick,现在时间：" + args.Hour + ":" + args.Minute + ":" + args.Second);
+        }
+        void OnAlarm1(object sender, TimeEventArgs args)
+        {
+            Console.WriteLine("Alarm，闹钟响了");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("为了实验方便将时间加速了10倍");
+            
+            MyClock myClockTest = new MyClock(25, 30);
+            myClockTest.myClock.GetClock();
+
+            myClockTest.myClock.SetAlarm(0, 1);
+            myClockTest.myClock.GetAlarm();
+
+            myClockTest.myClock.Run();
+        }
+    }
+}
+
---
+++ Cswork4/Print/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        public Node<T> head;
+        public Node<T> tail;
+
+        public GenericList()
+        {
+            head = tail = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail==null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<Node<T>> action)
+        {
+            Node<T> p = head;
+            while (p != null)
+            {
+                action(p);
+                p = p.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                intlist.Add(i);
+            }
+
+            intlist.ForEach(x => Console.Write(x.Data+" "));
+            Console.WriteLine();
+
+            int max = int.MinValue;
+            intlist.ForEach(x => max = x.Data > max ? x.Data : max);
+            Console.WriteLine($"max: {max}");
+
+            int min = int.MaxValue;
+            intlist.ForEach(x => min = x.Data < min ? x.Data : min);
+            Console.WriteLine($"min: {min}");
+
+            int sum = 0;
+            intlist.ForEach(x => sum += x.Data);
+            Console.WriteLine($"sum: {sum}");
+        }
+    }
+}
