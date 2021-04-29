6eb0b2c698ebd632a3eb744b351a08cb4043e13c
chnone <76799673+chnone@users.noreply.github.com>
2021-04-29 21:02:17 +0800

    Add files via upload

---
+++ Cswork7/Cayley/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace Cayley
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.n = new System.Windows.Forms.NumericUpDown();
+            this.length = new System.Windows.Forms.NumericUpDown();
+            this.per1 = new System.Windows.Forms.NumericUpDown();
+            this.per2 = new System.Windows.Forms.NumericUpDown();
+            this.th1 = new System.Windows.Forms.NumericUpDown();
+            this.th2 = new System.Windows.Forms.NumericUpDown();
+            this.pen = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.n)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.length)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.per1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.per2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.th1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.th2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.Transparent;
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1537, 1215);
+            this.splitContainer1.SplitterDistance = 462;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.n, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.length, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.per1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.per2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.th1, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.th2, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.pen, 1, 6);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(462, 1215);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "递归深度";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "主干长度";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "右分支长度比";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 516);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "左分支长度比";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 667);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "右分支角度";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 818);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "左分支角度";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 969);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 24);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "画笔颜色";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(234, 1060);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 152);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "开始绘制";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // n
+            // 
+            this.n.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.n.Location = new System.Drawing.Point(286, 58);
+            this.n.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.n.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(120, 35);
+            this.n.TabIndex = 8;
+            this.n.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // length
+            // 
+            this.length.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.length.DecimalPlaces = 1;
+            this.length.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.length.Location = new System.Drawing.Point(286, 209);
+            this.length.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(120, 35);
+            this.length.TabIndex = 9;
+            this.length.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // per1
+            // 
+            this.per1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.per1.DecimalPlaces = 1;
+            this.per1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.per1.Location = new System.Drawing.Point(286, 360);
+            this.per1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.per1.Name = "per1";
+            this.per1.Size = new System.Drawing.Size(120, 35);
+            this.per1.TabIndex = 10;
+            // 
+            // per2
+            // 
+            this.per2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.per2.DecimalPlaces = 1;
+            this.per2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.per2.Location = new System.Drawing.Point(286, 511);
+            this.per2.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.per2.Name = "per2";
+            this.per2.Size = new System.Drawing.Size(120, 35);
+            this.per2.TabIndex = 11;
+            // 
+            // th1
+            // 
+            this.th1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.th1.Location = new System.Drawing.Point(286, 662);
+            this.th1.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.th1.Name = "th1";
+            this.th1.Size = new System.Drawing.Size(120, 35);
+            this.th1.TabIndex = 12;
+            // 
+            // th2
+            // 
+            this.th2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.th2.Location = new System.Drawing.Point(286, 813);
+            this.th2.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.th2.Name = "th2";
+            this.th2.Size = new System.Drawing.Size(120, 35);
+            this.th2.TabIndex = 13;
+            // 
+            // pen
+            // 
+            this.pen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pen.FormattingEnabled = true;
+            this.pen.Items.AddRange(new object[] {
+            "红色",
+            "蓝色"});
+            this.pen.Location = new System.Drawing.Point(286, 965);
+            this.pen.Name = "pen";
+            this.pen.Size = new System.Drawing.Size(121, 32);
+            this.pen.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1537, 1215);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.n)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.length)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.per1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.per2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.th1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.th2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown n;
+        private System.Windows.Forms.NumericUpDown length;
+        private System.Windows.Forms.NumericUpDown per1;
+        private System.Windows.Forms.NumericUpDown per2;
+        private System.Windows.Forms.NumericUpDown th1;
+        private System.Windows.Forms.NumericUpDown th2;
+        private System.Windows.Forms.ComboBox pen;
+    }
+}
+
---
+++ Cswork7/Cayley/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cayley
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics graphics;
+        int N;
+        double Leng, Per1, Per2, Th1, Th2;
+        string Pen;
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.splitContainer1.Panel2.Refresh();
+
+            if (graphics == null) 
+                this.graphics = this.splitContainer1.Panel2.CreateGraphics();
+            
+            N = (int)n.Value;
+            Th1 = (int)th1.Value * Math.PI / 180;
+            Th2 = (int)th2.Value * Math.PI / 180;
+            Leng = (double)length.Value;
+            Per1 = (double)per1.Value;
+            Per2 = (double)per2.Value;
+            Pen = pen.Text;
+            drawCayleyTree(N, Leng, 290, 450, -Math.PI / 2);
+        }
+
+        void drawCayleyTree(int _n, double _leng, double x0, double y0, double _th)
+        {
+            if (_n == 0)
+                return;
+
+            double x1 = x0 + _leng * Math.Cos(_th);
+            double y1 = y0 + _leng * Math.Sin(_th);
+
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(_n - 1, x1, y1, Per1 * _leng, _th + Th1);
+            drawCayleyTree(_n - 1, x1, y1, Per2 * _leng, _th - Th2);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            switch (Pen)
+            {
+                case "红色":
+                    graphics.DrawLine(Pens.Red, (int)x0, (int)y0, (int)x1, (int)y1);
+                    break;
+                case "蓝色":
+                    graphics.DrawLine(Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+                    break;
+            }
+            
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
